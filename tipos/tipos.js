2f7e82bd04c5be43e56f9d80619d6b1fb33f3319
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-11-21 11:24:02 -0300

    correntista

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -3,6 +3,7 @@
 let nome = 'João';
 console.log(nome);
 //nome = 22;
+console.log('/////////////////////////////////////////////////////');
 //numbers
 let idade = 22;
 let idade2 = 22.5;
@@ -11,6 +12,7 @@ console.log(idade);
 //boolean
 let possuiHobbies = true;
 //possuiHobbies = 1;
+console.log('/////////////////////////////////////////////////////');
 console.log(possuiHobbies);
 //tipos explícitos 
 let minhaIdade;
@@ -22,17 +24,20 @@ console.log(minhaIdade);
 }else {
     console.log('falsoooo')
 } */
+console.log('/////////////////////////////////////////////////////');
 // array
 let hobbies = ["Cozinhar", "Praticar Esportes", 11];
 console.log(hobbies[0]);
 console.log(typeof hobbies);
 hobbies = [100];
 console.log(hobbies);
+console.log('/////////////////////////////////////////////////////');
 let endereco = ["Av principal", 99, 123];
 console.log(endereco);
 endereco = ["Rua Importante", 1256, 345];
 console.log(endereco);
 //enums
+console.log('/////////////////////////////////////////////////////');
 var Cor;
 (function (Cor) {
     Cor[Cor["Cinza"] = 0] = "Cinza";
@@ -44,21 +49,25 @@ let minhaCor = Cor.Verde;
 console.log(minhaCor);
 console.log(Cor.Azul);
 console.log(Cor.Vermelho, Cor.Azul);
+console.log('/////////////////////////////////////////////////////');
 //any
 let carro = 'BMW';
 console.log(carro);
 carro = { marca: 'BMW', ano: 2019 };
 console.log(carro);
+console.log('/////////////////////////////////////////////////////');
 function digaOi() {
     console.log('Oi');
 }
 digaOi();
+console.log('/////////////////////////////////////////////////////');
 function multiplicar(numA, numB) {
     return numA * numB;
 }
 /* console.log(multiplicar(2,'bia'))
  */
 console.log(multiplicar(4.75, 9));
+console.log('/////////////////////////////////////////////////////');
 //objetos
 let usuarios = {
     nome: 'Samuel',
@@ -72,6 +81,7 @@ usuarios = {
     idade: 28
 };
 console.log(usuarios);
+console.log('/////////////////////////////////////////////////////');
 let funcionario = {
     supervisores: ['Ana', 'Fernando'],
     baterPonto(horario) {
@@ -86,6 +96,7 @@ let funcionario = {
 console.log(funcionario.supervisores);
 console.log(funcionario.baterPonto(8));
 console.log(funcionario.baterPonto(12));
+console.log('/////////////////////////////////////////////////////');
 let funcionario2 = {
     supervisores: ['Bia', 'Carlos'],
     baterPonto(horario) {
@@ -100,3 +111,48 @@ let funcionario2 = {
 console.log(funcionario2.supervisores);
 console.log(funcionario.baterPonto(8));
 console.log(funcionario.baterPonto(12));
+console.log('/////////////////////////////////////////////////////');
+//union types
+let nota = 10;
+console.log(`Minha nota é ${nota}!`);
+nota = '100';
+console.log(`Minha nota é ${nota}!`);
+console.log('/////////////////////////////////////////////////////');
+//checando tipos
+let valor = 23;
+if (typeof valor == "number") {
+    console.log("É um number!");
+}
+else {
+    console.log(typeof valor);
+}
+console.log('/////////////////////////////////////////////////////');
+let altura = 12;
+let alturaOpcional = 12;
+alturaOpcional = null;
+const contato1 = {
+    nome: 'Fulano',
+    tel1: '987654321',
+    tel2: null
+};
+console.log(contato1.nome);
+console.log(contato1.tel1);
+console.log(contato1.tel2);
+let podeSerNulo = null;
+podeSerNulo = 12;
+console.log(podeSerNulo);
+podeSerNulo = 'Maria';
+console.log(podeSerNulo);
+let ContaBancaria = {
+    saldo: 3456,
+    depositar(valor) {
+        this.saldo += valor;
+    }
+};
+let Correntista = {
+    nome: 'Ana Silva',
+    ContaBancaria: ContaBancaria,
+    contatos: ['32567890', '98765432']
+};
+Correntista.ContaBancaria.depositar(90000);
+console.log(Correntista);
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -2,6 +2,7 @@
 let nome:string = 'João';
 console.log(nome);
 //nome = 22;
+console.log('/////////////////////////////////////////////////////')
 
 //numbers
 let idade:number = 22;
@@ -12,6 +13,7 @@ console.log(idade);
 //boolean
 let possuiHobbies:boolean = true;
 //possuiHobbies = 1;
+console.log('/////////////////////////////////////////////////////')
 
 console.log(possuiHobbies);
 
@@ -29,6 +31,7 @@ console.log(minhaIdade);
 }else {
     console.log('falsoooo')
 } */
+console.log('/////////////////////////////////////////////////////')
 
 // array
 let hobbies:any[] = ["Cozinhar","Praticar Esportes",11]
@@ -37,6 +40,7 @@ console.log(typeof hobbies)
 
 hobbies = [100]
 console.log(hobbies)
+console.log('/////////////////////////////////////////////////////')
 
 let endereco: [string,number,number] = ["Av principal",99, 123]
 console.log(endereco)
@@ -45,6 +49,7 @@ endereco = ["Rua Importante",1256,345]
 console.log(endereco)
 
 //enums
+console.log('/////////////////////////////////////////////////////')
 
 enum Cor {
     Cinza, //0
@@ -58,6 +63,7 @@ console.log(minhaCor)
 console.log(Cor.Azul)
 console.log(Cor.Vermelho, Cor.Azul)
 
+console.log('/////////////////////////////////////////////////////')
 
 //any
 let carro: any = 'BMW'
@@ -65,12 +71,14 @@ console.log(carro)
 carro = {marca: 'BMW',ano:2019}
 console.log(carro)
 
+console.log('/////////////////////////////////////////////////////')
 
 
 function digaOi():void {
     console.log('Oi')
 }
 digaOi()
+console.log('/////////////////////////////////////////////////////')
 
 function multiplicar(numA:number, numB:number):number {
     return numA * numB
@@ -78,6 +86,7 @@ function multiplicar(numA:number, numB:number):number {
 /* console.log(multiplicar(2,'bia'))
  */
 console.log(multiplicar(4.75,9))
+console.log('/////////////////////////////////////////////////////')
 
 
 //objetos
@@ -96,6 +105,7 @@ usuarios = {
     idade:28
 }
 console.log(usuarios)
+console.log('/////////////////////////////////////////////////////')
 
 /* usuarios = {
     name: 'Maria',
@@ -122,6 +132,7 @@ console.log(funcionario.supervisores)
 console.log(funcionario.baterPonto(8))
 console.log(funcionario.baterPonto(12))
 
+console.log('/////////////////////////////////////////////////////')
 
 
 
@@ -138,3 +149,82 @@ let funcionario2 :Funcionariotype = {
 console.log(funcionario2.supervisores)
 console.log(funcionario.baterPonto(8))
 console.log(funcionario.baterPonto(12))
+console.log('/////////////////////////////////////////////////////')
+
+
+//union types
+let nota:number | string = 10
+
+console.log(`Minha nota é ${nota}!`)
+nota = '100'
+console.log(`Minha nota é ${nota}!`)
+console.log('/////////////////////////////////////////////////////')
+
+//checando tipos
+let valor = 23
+if(typeof valor == "number"){
+    console.log("É um number!")
+}else {
+    console.log(typeof valor)
+}
+
+console.log('/////////////////////////////////////////////////////')
+
+
+let altura = 12
+
+let alturaOpcional:null | number = 12
+
+alturaOpcional = null
+
+type Contato = {
+    nome: string,
+    tel1: string,
+    tel2: string | null
+}
+
+const contato1: Contato = {
+    nome: 'Fulano',
+    tel1: '987654321',
+    tel2: null
+}
+
+console.log(contato1.nome)
+console.log(contato1.tel1)
+console.log(contato1.tel2)
+
+
+let podeSerNulo = null
+
+podeSerNulo = 12
+console.log(podeSerNulo)
+podeSerNulo = 'Maria'
+console.log(podeSerNulo)
+
+type ContaBancaria = {
+    saldo: number,
+    depositar:(valor: number) => void
+}
+
+
+let ContaBancaria: ContaBancaria = {
+    saldo: 3456,
+    depositar(valor: number){
+        this.saldo += valor
+    }
+}
+
+type Correntista = {
+    nome: string,
+    ContaBancaria: ContaBancaria,
+    contatos: string[]
+}
+
+let Correntista: Correntista = {
+    nome: 'Ana Silva',
+    ContaBancaria: ContaBancaria,
+    contatos: ['32567890','98765432']
+}
+
+Correntista.ContaBancaria.depositar(90000);
+console.log(Correntista);
