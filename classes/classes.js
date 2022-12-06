ed85a0807e93c59c125e8b91bf141076c861401f
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-12-06 13:38:14 -0300

    ADd

--- classes/classes.js
+++ classes/classes.js
@@ -30,4 +30,16 @@ console.log(aniversarioEsperta);
 const casamentoEsperta = new Data; //posso omitir os "()"
 casamentoEsperta.ano = 2022;
 console.log(casamentoEsperta);
+class Produto {
+    constructor(nome, preco, desconto = 0) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+}
+const prod1 = new Produto('Caneta Bic Preta', 4.20);
+prod1.desconto = 0.06;
+console.log(prod1);
+const prod2 = new Produto('Caderno Escolar', 18.80, 0.15);
+console.log(prod2);
 //# sourceMappingURL=classes.js.map
--- classes/classes.ts
+++ classes/classes.ts
@@ -38,3 +38,17 @@ console.log(aniversarioEsperta)
 const casamentoEsperta = new Data //posso omitir os "()"
 casamentoEsperta.ano = 2022
 console.log(casamentoEsperta)
+
+
+class Produto {
+    constructor(public nome:string, public preco:number, public desconto: number = 0){
+
+    }
+}
+
+const prod1 = new Produto ('Caneta Bic Preta', 4.20)
+prod1.desconto = 0.06
+console.log(prod1)
+
+const prod2 = new Produto ('Caderno Escolar', 18.80,0.15)
+console.log(prod2)
